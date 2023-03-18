6ca9979a13d3ca78b9f2df01d51f65f8e4b406f0
desfint <81056093+desfint@users.noreply.github.com>
2023-03-18 13:46:07 -0400

    Functional image to ascii cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use image::{io::Reader, ImageResult, imageops::FilterType::CatmullRom};
+
+fn main() -> ImageResult<()> {
+    const BRIGHTNESS_CHARS: [char; 64] = [
+        ' ', '^', '"', ',', ':', ';', 'I', 'l', '!', 'i', '~', '+', '_', '-', '?', ']', '[', '}', '{', '1', ')', '(', '|', '/' ,'t' ,'f' ,'j', 'r', 'x', 'n', 'u', 'v', 'c', 'z', 'X', 'Y', 'U', 'J', 'C', 'L', 'Q', '0', 'O', 'Z', 'm', 'w', 'q', 'p', 'd', 'b', 'k', 'h', 'a', 'o', '*', '#', 'M', 'W', '&', '8', '%', 'B', '@', '$'
+    ];
+
+    let file_path = std::env::args().nth(1).expect("No file given");
+
+    let mut image = Reader::open(file_path)?
+        .with_guessed_format()?
+        .decode()?;
+
+    if image.width() > 1000 {
+        image = image.resize(1400, image.height(), CatmullRom);
+    }
+
+    let (width, height) = (image.width(), image.height());
+    let image = image.resize_exact(width, height/3, CatmullRom);
+    let (width, height) = (image.width(), image.height());
+    let image_bytes = image.as_bytes();
+    let pixel_size = image_bytes.len() / (width * height) as usize;
+    let pixels: Vec<u8> = image_bytes
+        .chunks_exact(pixel_size)
+        .map(|pixel| {
+            (pixel.iter()
+            .fold(0u32, |acc, n| {
+                acc + *n as u32
+            }) / pixel.len() as u32) as u8
+        })
+        .collect();
+
+    pixels.chunks_exact(width as usize).for_each(|chunk| {
+        chunk.iter().for_each(|pixel| {
+            let brightness = pixel / 4;
+            print!("{}", BRIGHTNESS_CHARS[brightness as usize]);
+        });
+        println!(" ");
+    });
+
+    Ok(())
 }
